f06f9592b3e74717f7e12bf16e4ac18055745727
LathithaKunana <lathitha.kunana@younglings.africa>
2023-08-14 16:09:56 +0200

    added src folder with all of the neccesary files

---
+++ src/views/screens/LoginScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const LoginScreen = ({navigation}) => {
+    return (
+        <SafeAreaView></SafeAreaView>
+    );
+};
+
+export default LoginScreen;
---
+++ src/views/screens/RegistrationScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const RegistrationScreen = ({navigation}) => {
+    return (
+      <SafeAreaView></SafeAreaView>
+    );
+}
+
+export default RegistrationScreen;
