a5aa00f9f46fbfa7280f38289bad0b9f1bda3bc7
LathithaKunana <lathitha.kunana@younglings.africa>
2023-08-16 16:29:44 +0200

    updated app

--- src/views/screens/RegistrationScreen.js
+++ src/views/screens/RegistrationScreen.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, ScrollView, Text, } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const RegistrationScreen = ({navigation}) => {
     return (
-      <SafeAreaView></SafeAreaView>
+      <SafeAreaView  style ={{backgroundColor: 'white', flex: 1}}>
+        <ScrollView
+          contentContainerStyle={{
+            paddingTop: 50,
+            paddingHorizontal: 20,
+          }}>
+            <Text style={{color: 'black', fontSize: 40, fontWeight: 'bold'}}>
+              Register
+            </Text>
+          
+        </ScrollView>
+
+      </SafeAreaView>
     );
 }
 
